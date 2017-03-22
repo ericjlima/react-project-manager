a43c7027d2754b6cbdbc26eae09353cad5cf0896
Eric Lima <ericjlima@gmail.com>
2017-03-21 22:31:26 -0400

    added the abilitiy to create and delete projects

--- src/App.js
+++ src/App.js
@@ -1,5 +1,7 @@
 import React, { Component } from 'react';
+import uuid from 'uuid';
 import Projects from './Components/Projects';
+import AddProject from './Components/AddProject';
 import './App.css';
 
 class App extends Component {
@@ -7,27 +9,50 @@ class App extends Component {
     super();
     this.state = {
       projects: [
-        {
-          title: 'Business Website',
-          category: 'Web Design'
-        },
-        {
-          title: 'Social App',
-          category: 'Mobile Development'
-        },
-        {
-          title: 'Ecommerce Shopping Cart',
-          category: 'Web Development'
-        }
+
       ]
     }
+  }
+
+  componentWillMount(){
+    this.setState({projects: [
+      {
+        id: uuid.v4(),
+        title: 'Business Website',
+        category: 'Web Design'
+      },
+      {
+        id: uuid.v4(),
+        title: 'Social App',
+        category: 'Mobile Development'
+      },
+      {
+        id: uuid.v4(),
+        title: 'Ecommerce Shopping Cart',
+        category: 'Web Development'
+      }
+    ]});
+  }
+
+  handleAddProject(project){
+    let projects = this.state.projects;
+    projects.push(project);
+    this.setState({projects: projects});
+  }
+
+  handleDeleteProject(id){
+    let projects = this.state.projects;
+    let index = projects.findIndex(x => x.id === id);
+    projects.splice(index, 1);
+    this.setState({projects:projects});
+
   }
 
   render() {
     return (
       <div className="Projects">
-        My App
-        <Projects projects={this.state.projects} />
+        <AddProject addProject={this.handleAddProject.bind(this)}/>
+        <Projects onDelete={this.handleDeleteProject.bind(this)} projects={this.state.projects} />
       </div>
     );
   }
--- src/Components/Projects.js
+++ src/Components/Projects.js
@@ -1,10 +1,25 @@
 import React, { Component } from 'react';
+import ProjectItem from './ProjectItem';
 
 class Projects extends Component {
+  deleteProject(id){
+    this.props.onDelete(id);
+  }
   render() {
-    return ( 
+    let projectItems;
+    if(this.props.projects){
+      projectItems = this.props.projects.map(project => {
+        //console.log(project);
+        return(
+          <ProjectItem onDelete={this.deleteProject.bind(this)} key={project.title} project={project} />
+        );
+      });
+    }
+    //console.log(this.props);
+    return (
       <div className="Projects">
-        My Projects
+        <h3>Latest Projects</h3>
+        {projectItems}
       </div>
     );
   }
